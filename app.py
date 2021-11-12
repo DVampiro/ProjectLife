77eaffa5aa944e8d45f5ae0a4f2cc0ff2ee96d53
Dmitry <mmmmm@mmmmm.mmmm>
2021-11-12 17:25:16 +0500

    Рабочий проект без модификаций

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+import game_of_life
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index_page():
+    game_of_life.GameOfLife()
+    return render_template('index.html')
+
+
+@app.route('/live')
+def live_page():
+    game = game_of_life.GameOfLife()
+    if game.counter:
+        game.form_new_generation()
+    game.counter += 1
+    return render_template('live.html', game=game)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=False)
